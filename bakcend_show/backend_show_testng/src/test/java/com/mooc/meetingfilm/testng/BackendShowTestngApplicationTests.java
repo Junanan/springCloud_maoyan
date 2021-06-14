134009da1c7c42fdba6c908b67084a7d234c65e1
Junanan <1161944970@qq.com>
2021-06-14 22:36:06 +0800

    finish

---
+++ bakcend_show/backend_show_testng/src/test/java/com/mooc/meetingfilm/testng/BackendShowTestngApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mooc.meetingfilm.testng;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShowTestngApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
